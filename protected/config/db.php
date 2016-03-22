31950bc7929b45af11c3dcd5cff1072cf6300164
Bui Sy Nguyen <nguyenbs@projectkit.net>
2016-03-22 22:15:39 +0700

    First compile OK

---
+++ protected/config/db.php
@@ -0,0 +1,17 @@
+<?php
+///////////////////////////////////////////////////////////////////////////////
+//
+// Licensed Source Code - Property of f-project.net
+//
+// © Copyright f-project.net 2015. All Rights Reserved.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+return [
+    'connectionString' => 'mysql:host=localhost;dbname=u168748988_sec',//'mysql:host=mysql.hostinger.vn;dbname=u997817970_pk',
+    'emulatePrepare' => true,
+    'username' => 'root',//Use 'u997817970_pk' for test.f-project.net
+    'password' => 'root',//Use 'pk_13245' for test.f-project.net
+    'charset' => 'utf8',
+    //'enableParamLogging'=>true,
+];
